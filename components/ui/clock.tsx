ae981f4052658ea599dd1d083f9e4f0c38171322
LongTran <tranphilongnvd@gmail.com>
2025-09-28 10:21:53 +0700

    feat: implement LiveClock component with styling and number rendering

--- components/ui/clock.tsx
+++ components/ui/clock.tsx
@@ -1,19 +1,19 @@
-"use client";
-
 import { useEffect, useState } from "react";
 import Clock from "react-clock";
 import "react-clock/dist/Clock.css";
+import "./clock.css";
 
 export default function LiveClock() {
   const [value, setValue] = useState(new Date());
 
   useEffect(() => {
     const interval = setInterval(() => setValue(new Date()), 1000);
-
-    return () => {
-      clearInterval(interval);
-    };
+    return () => clearInterval(interval);
   }, []);
 
-  return <Clock value={value} />;
+  return (
+    <div className="flex items-center justify-center">
+      <Clock value={value} renderNumbers={true} className="!w-48 !h-48" />
+    </div>
+  );
 }
