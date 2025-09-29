27afd265f0048213929ce7e67c3d9df62e648265
LongTran <tranphilongnvd@gmail.com>
2025-09-29 23:05:05 +0700

    feat: update UUID logic

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,8 +5,16 @@ export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
 };
 
+const generateUUIDv4 = (): string => {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (char) => {
+    const random: number = Math.floor(Math.random() * 16); // 0–15
+    const value: number = char === "x" ? random : (random & 0x3) | 0x8;
+    return value.toString(16);
+  });
+};
+
 export const generateId = () => {
-  return crypto.randomUUID();
+  return generateUUIDv4();
 };
 
 export const dbPaths = {
